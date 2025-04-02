d64cc531560b7f211188df6f2d919d721c19e077
alagorioGIT <alejandro.lagorio@gmail.com>
2025-04-02 19:00:12 -0300

    cookies

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-let moviesRouter=require('./routes/movie');
-let faqsRouter=require('./routes/faqs');
-let aboutUsRouter=require('./routes/aboutUs');
+let moviesRouter = require('./routes/movie');
+let faqsRouter = require('./routes/faqs');
+let aboutUsRouter = require('./routes/aboutUs');
 
 
 var app = express();
@@ -23,6 +24,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'dbmovies',
+  resave: false,
+  saveUninitialized: true
+}));
+
+
+// Middleware para guardar la ultima pelicula en la session
+app.use((req, res, next) => {
+  console.log('Time:', Date.now())
+  next()
+});
+
+
+app.use(function (req, res, next) {
+  console.log("Cookie "+ req.cookies.lastMovie);
+  if (req.session.lastMovie !== undefined) {
+    res.locals.lastMovie = req.session.lastMovie; //almacena la ultima pelicula en la session
+    console.log("Preuba Session "+ res.locals.lastMovie.title); //muestra la info en el navegador
+  }
+  return next();//Esto es para que la aplicacion siga su curso
+});
+//Rutas
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/movies', moviesRouter);
@@ -30,12 +55,12 @@ app.use('./faqs', faqsRouter);
 app.use('./aboutUs', aboutUsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- controlers/moviesControlers.js
+++ controlers/moviesControlers.js
@@ -1,21 +1,24 @@
+const session = require("express-session");
+
 let moviesControlers = {
-    index: function(req, res){
+    index: function (req, res) {
         let movieList = ['Rocky', 'Batman', 'Barbie', 'Iron Man'];
-        return res.render('movies', { title: 'Movies', listaPelis: movieList});
+        return res.render('movies', { title: 'Movies', listaPelis: movieList });
     },
     show: function (req, res) {
         return res.send(`Estamos en el deatalle de la pelicula:  ${req.params.id} `)
     },
-    create: function(req,res){
-        return res.render('movieNew',{title:'Nuew Peli'})
+    create: function (req, res) {
+        return res.render('movieNew', { title: 'Nuew Peli' })
     },
-    search: function(req, res){
-        let searchTerm= req.query.search;
-        return res.render('searchResults', {title: 'Resultados de la busqueda', searchTerm});;
+    search: function (req, res) {
+        let searchTerm = req.query.search;
+        return res.render('searchResults', { title: 'Resultados de la busqueda', searchTerm });;
     },
-    store: function(req, res){
-        let info= req.body; //en body estan las propiedades del formulario
-        // return res.send(info); //envia la info al navegador
+    store: function (req, res) {
+        let info = req.body; //en body estan las propiedades del formulario
+        req.session.lastMovie = info; //almacena la ultima pelicula en la session 
+        res.cookie('lastMovie', info.title,{maxAge:1000*60*5}); //almacena la ultima pelicula en la cookie
         return res.redirect('/'); //redirecciona a la ruta principal hasta que se cree la base de datos
     }
 
